b06ddab4b3c7255b88309a8ee7fed431ed1a7578
Daniel Nikolaev <toolsproed@gmail.com>
2025-02-15 21:04:30 +0200

    moved EditorLine test in the line mod

--- src/workspace/renderer/code/tests.rs
+++ src/workspace/renderer/code/tests.rs
@@ -9,182 +9,9 @@ use crate::syntax::tests::{
     zip_text_tokens,
 };
 use crate::workspace::cursor::Cursor;
-use crate::workspace::line::{EditorLine, LineContext};
+use crate::workspace::line::LineContext;
 use crate::workspace::CursorPosition;
 use crossterm::style::ContentStyle;
-
-#[test]
-fn test_insert() {
-    let mut line = EditorLine::new("text".to_owned());
-    assert!(line.char_len() == 4);
-    line.insert(2, 'e');
-    assert!(line.is_simple());
-    line.insert(2, '🚀');
-    assert!(line.char_len() == 6);
-    assert!(!line.is_simple());
-    line.insert(3, 'x');
-    assert!(line.char_len() == 7);
-    assert!(&line.to_string() == "te🚀xext");
-}
-
-#[test]
-fn test_insert_str() {
-    let mut line = EditorLine::new("text".to_owned());
-    line.insert_str(0, "text");
-    assert!(line.is_simple());
-    assert!(line.char_len() == 8);
-    line.insert_str(1, "rocket🚀");
-    assert!(!line.is_simple());
-    assert!(&line.to_string() == "trocket🚀exttext");
-    assert!(line.char_len() < line.to_string().len());
-}
-
-#[test]
-fn test_push() {
-    let mut line = EditorLine::new("text".to_owned());
-    line.push('1');
-    assert!(line.is_simple());
-    assert!(line.char_len() == 5);
-    line.push('🚀');
-    assert!(!line.is_simple());
-    assert!(line.to_string().len() == 9);
-    assert!(line.char_len() == 6);
-    assert!(&line.to_string() == "text1🚀");
-}
-
-#[test]
-fn test_push_str() {
-    let mut line = EditorLine::new(String::new());
-    assert!(line.is_simple());
-    assert!(line.char_len() == 0);
-    line.push_str("text");
-    assert!(line.is_simple());
-    assert!(line.char_len() == 4);
-    line.push_str("text🚀");
-    assert!(!line.is_simple());
-    assert!(&line.to_string() == "texttext🚀");
-    assert!(line.char_len() == 9);
-    assert!(line.to_string().len() == 12);
-}
-
-#[test]
-fn test_replace_range() {
-    let mut line = EditorLine::new(String::from("🚀123"));
-    assert!(!line.is_simple());
-    assert!(line.char_len() == 4);
-    line.replace_range(0..2, "text");
-    assert!(line.is_simple());
-    assert!(&line.to_string() == "text23");
-    assert!(line.char_len() == 6);
-    line.replace_range(3..6, "🚀🚀");
-    assert!(!line.is_simple());
-    assert!(&line.to_string() == "tex🚀🚀");
-    assert!(line.char_len() == 5);
-}
-
-#[test]
-fn test_replace_till() {
-    let mut line = EditorLine::new(String::from("🚀123"));
-    assert!(!line.is_simple());
-    assert!(line.char_len() == 4);
-    line.replace_till(3, "text");
-    assert!(line.is_simple());
-    assert!(&line.to_string() == "text3");
-    assert!(line.char_len() == 5);
-    line.replace_till(2, "🚀🚀");
-    assert!(!line.is_simple());
-    assert!(&line.to_string() == "🚀🚀xt3");
-    assert!(line.char_len() == 5);
-}
-
-#[test]
-fn test_replace_from() {
-    let mut line = EditorLine::new(String::from("123🚀"));
-    assert!(!line.is_simple());
-    assert!(line.char_len() == 4);
-    line.replace_from(3, "text");
-    assert!(line.is_simple());
-    assert!(line.char_len() == 7);
-    assert!(&line.to_string() == "123text");
-    line.replace_from(3, "🚀🚀");
-    assert!(!line.is_simple());
-    assert!(line.char_len() == 5);
-    assert!(&line.to_string() == "123🚀🚀");
-}
-
-#[test]
-fn test_remove() {
-    let mut line = EditorLine::new("text🚀123".to_owned());
-    assert!(!line.is_simple());
-    assert!(line.char_len() == 8);
-    assert!('1' == line.remove(5));
-    assert!(line.char_len() == 7);
-    assert!(!line.is_simple());
-    assert!('🚀' == line.remove(4));
-    assert!(line.is_simple());
-    assert!(line.char_len() == 6);
-    assert!(&line.to_string() == "text23");
-}
-
-#[test]
-fn test_utf8_idx_at() {
-    let line = EditorLine::new("text🚀123🚀".to_owned());
-    assert_eq!(4, line.unsafe_utf8_idx_at(4));
-    assert_eq!(2, line.unsafe_utf8_idx_at(2));
-    assert_eq!(8, line.unsafe_utf8_idx_at(5));
-    assert_eq!(10, line.unsafe_utf8_idx_at(7));
-    assert_eq!(15, line.unsafe_utf8_idx_at(9));
-}
-
-#[test]
-#[should_panic]
-fn test_utf8_idx_at_panic() {
-    let line = EditorLine::new("text🚀123🚀".to_owned());
-    line.unsafe_utf8_idx_at(10);
-}
-
-#[test]
-fn test_utf16_idx_at() {
-    let line = EditorLine::new("text🚀123🚀".to_owned());
-    assert_eq!(4, line.unsafe_utf16_idx_at(4));
-    assert_eq!(2, line.unsafe_utf16_idx_at(2));
-    assert_eq!(6, line.unsafe_utf16_idx_at(5));
-    assert_eq!(8, line.unsafe_utf16_idx_at(7));
-    assert_eq!(11, line.unsafe_utf16_idx_at(9));
-}
-
-#[test]
-#[should_panic]
-fn test_utf16_idx_at_panic() {
-    let line = EditorLine::new("text🚀123🚀".to_owned());
-    line.unsafe_utf16_idx_at(10);
-}
-
-#[test]
-fn test_split_off() {
-    let mut line = EditorLine::new("text🚀123🚀".to_owned());
-    line = line.split_off(2);
-    assert_eq!(line.to_string(), "xt🚀123🚀");
-    assert_eq!(line.char_len(), 7);
-    assert_eq!(line.len(), 13);
-    let new = line.split_off(4);
-    assert_eq!(new.char_len(), 3);
-    assert_eq!(new.len(), 6);
-    assert_eq!(new.unwrap(), "23🚀");
-}
-
-#[test]
-fn test_split_off_ascii() {
-    let mut line = EditorLine::new("texttext".to_owned());
-    let remaining = line.split_off(4);
-    assert_eq!(remaining.char_len(), 4);
-    assert_eq!(remaining.len(), 4);
-    assert_eq!(remaining.to_string(), "text");
-    assert_eq!(line.char_len(), 4);
-    assert_eq!(line.len(), 4);
-    assert_eq!(line.to_string(), "text");
-    assert_eq!(line.to_string(), "text");
-}
 
 /// LINE RENDER
 
@@ -500,7 +327,6 @@ fn parse_complex_line(rendered: &mut Vec<(ContentStyle, String)>) -> (Option<usi
     (line_idx, parsed)
 }
 
-#[inline]
 fn test_content(mut render_data: Vec<(ContentStyle, String)>) {
     let (line_num, line) = parse_simple_line(&mut render_data);
     assert_eq!(line_num, Some(1));
@@ -577,7 +403,6 @@ fn test_content(mut render_data: Vec<(ContentStyle, String)>) {
     assert_eq!(line, vec!["}"]);
 }
 
-#[inline]
 fn test_content_select(mut render_data: Vec<(ContentStyle, String)>) {
     let (line_num, line) = parse_simple_line(&mut render_data);
     assert_eq!(line_num, Some(1));
