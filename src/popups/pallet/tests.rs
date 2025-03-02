784c2909e504a836ebf540b77de87f6eb506168f
Daniel Nikolaev <toolsproed@gmail.com>
2025-03-02 20:56:40 +0200

    removed fake test

--- src/popups/pallet/tests.rs
+++ src/popups/pallet/tests.rs
@@ -1,10 +1,1 @@
-use crate::configs::CONFIG_FOLDER;
-use dirs::config_dir;
 
-#[test]
-fn bumb() {
-    let mut path = config_dir().unwrap();
-    path.push(CONFIG_FOLDER);
-    path.push("data");
-    panic!("{:?}", path);
-}
