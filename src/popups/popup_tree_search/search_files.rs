c4a6c7ae56ac98b96284acecac81440eda8cac16
Daniel Nikolaev <toolsproed@gmail.com>
2025-04-20 11:12:16 +0300

    new version of cache search in file

--- src/popups/popup_tree_search/search_files.rs
+++ src/popups/popup_tree_search/search_files.rs
@@ -1,7 +1,7 @@
-use super::{Components, Popup, Status, WAIT_ON_UPDATE};
+use super::{Components, Popup, Status};
 use crate::{
     embeded_term::EditorTerminal,
-    global_state::{GlobalState, IdiomEvent},
+    global_state::GlobalState,
     render::{
         backend::StyleExt,
         layout::{LineBuilder, Rect, BORDERS},
@@ -13,9 +13,11 @@ use crate::{
 };
 use crossterm::event::{KeyCode, KeyEvent, MouseButton, MouseEvent, MouseEventKind};
 use crossterm::style::Color;
-use std::time::Instant;
-use std::{path::PathBuf, sync::Arc};
-use tokio::{sync::Mutex as AsyncMutex, task::JoinHandle};
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
+use tokio::task::JoinSet;
+
+const MAX_SEARCH_TIME: Duration = Duration::from_millis(50);
 
 type SearchResult = (PathBuf, String, usize);
 
@@ -35,29 +37,110 @@ const FILE_SEARCH_TITLE: SearchMode =
 
 const FULL_SEARCH_TITLE: SearchMode = SearchMode { title: "File search (root)", fg_color: Color::Red };
 
+struct CachedBuffer {
+    base_search: String,
+    filter: String,
+    base_line: Vec<SearchResult>,
+    tasks: Option<JoinSet<Vec<SearchResult>>>,
+    tree: TreePath,
+}
+
+impl CachedBuffer {
+    fn new(tree: TreePath) -> Self {
+        Self { base_search: String::new(), filter: String::new(), base_line: Vec::new(), tasks: None, tree }
+    }
+
+    fn rebase_cache(&mut self) {
+        _ = self.tasks.take().map(|mut t| t.abort_all());
+        self.tasks = Some(self.tree.clone().search_files_join_set(self.base_search.to_owned()));
+    }
+
+    fn len(&self) -> usize {
+        if self.filter.is_empty() {
+            return self.base_line.len();
+        }
+        self.base_line.iter().filter(|res| res.1.contains(self.filter.as_str())).count()
+    }
+
+    fn is_empty(&self) -> bool {
+        if self.filter.is_empty() {
+            return self.base_line.is_empty();
+        }
+        !self.base_line.iter().any(|res| res.1.contains(self.filter.as_str()))
+    }
+
+    fn vec(&mut self) -> Vec<SearchResult> {
+        self.flush_results();
+        self.base_line.iter().filter(|res| res.1.contains(self.filter.as_str())).cloned().collect()
+    }
+
+    fn is_running(&self) -> bool {
+        self.tasks.as_ref().map(|t| !t.is_empty()).unwrap_or_default()
+    }
+
+    fn set_search(&mut self, pattern: &str) {
+        if pattern.len() < 2 {
+            _ = self.tasks.take().map(|mut t| t.abort_all());
+            self.base_line.clear();
+            self.base_search.clear();
+            self.filter.clear();
+            return;
+        }
+        if self.base_search.is_empty() || !pattern.starts_with(&self.base_search) {
+            self.base_search = pattern.to_owned();
+            self.rebase_cache();
+        } else {
+            self.filter = pattern.to_owned();
+        }
+    }
+
+    fn new_tree(&mut self, tree: TreePath, pattern: &str) {
+        // drop existing tasks
+        _ = self.tasks.take().map(|mut t| t.abort_all());
+        // replace self with clean buffer
+        *self = Self::new(tree);
+        self.set_search(pattern);
+    }
+
+    fn flush_results(&mut self) {
+        let Some(tasks) = self.tasks.as_mut() else { return };
+        if tasks.is_empty() {
+            self.tasks = None;
+            return;
+        }
+        let start = Instant::now();
+        loop {
+            if start.elapsed() > MAX_SEARCH_TIME {
+                return;
+            }
+            let Some(result) = tasks.try_join_next() else { return };
+            if let Ok(data) = result {
+                self.base_line.extend(data);
+            }
+        }
+    }
+}
+
 pub struct ActiveFileSearch {
-    join_handle: Option<JoinHandle<()>>,
-    options: Vec<SearchResult>,
-    option_buffer: Arc<AsyncMutex<Vec<SearchResult>>>,
     state: State,
     mode: SearchMode,
     pattern: TextField<bool>,
-    clock: Option<Instant>,
-    tree: TreePath,
+    buffer: CachedBuffer,
 }
 
 impl ActiveFileSearch {
     pub fn run(pattern: String, gs: &mut GlobalState, ws: &mut Workspace, tree: &mut Tree, term: &mut EditorTerminal) {
-        let clock = if pattern.len() > 2 { Some(Instant::now()) } else { None };
+        let path_tree = tree.shallow_copy_selected_tree_path();
+        let mut buffer = CachedBuffer::new(path_tree);
+        if pattern.len() >= 2 {
+            buffer.set_search(&pattern);
+        }
+
         let mut new = Self {
             mode: FILE_SEARCH_TITLE,
-            join_handle: None,
-            option_buffer: Arc::default(),
-            options: Vec::default(),
             state: State::default(),
             pattern: TextField::new(pattern, Some(true)),
-            clock,
-            tree: tree.shallow_copy_selected_tree_path(),
+            buffer,
         };
 
         if new.pattern.text.len() > 1 {
@@ -68,18 +151,8 @@ impl ActiveFileSearch {
     }
 
     fn collect_data(&mut self) {
-        if let Some(handle) = self.join_handle.take() {
-            handle.abort();
-        }
-
-        self.options.clear();
         self.state.reset();
-
-        if self.pattern.text.len() < 2 {
-            return;
-        };
-
-        self.clock = Some(Instant::now());
+        self.buffer.set_search(&self.pattern.text);
     }
 
     fn get_rect(gs: &GlobalState) -> Rect {
@@ -92,7 +165,7 @@ impl ActiveFileSearch {
         rect.row += 2;
         let position = rect.relative_position(row, column)?;
         let idx = (self.state.at_line + position.line) / 2;
-        if idx >= self.options.len() {
+        if idx >= self.buffer.len() {
             return None;
         }
         Some(idx)
@@ -113,15 +186,14 @@ impl Popup for ActiveFileSearch {
         let Some(line) = rect.next_line() else { return };
         line.fill(BORDERS.horizontal_top, backend);
 
-        if self.clock.is_some() || self.join_handle.is_some() {
-            self.state.render_list(["Searching ..."].into_iter(), rect, backend);
-            return;
-        }
-
-        if self.options.is_empty() {
-            self.state.render_list(["No results found!"].into_iter(), rect, backend);
+        if self.buffer.is_empty() {
+            if self.buffer.is_running() {
+                self.state.render_list(["Searching ..."].into_iter(), rect, backend);
+            } else {
+                self.state.render_list(["No results found!"].into_iter(), rect, backend);
+            }
         } else {
-            self.state.render_list_complex(&self.options, &[build_path_line, build_text_line], rect, backend);
+            self.state.render_list_complex(&self.buffer.vec(), &[build_path_line, build_text_line], rect, backend);
         }
     }
 
@@ -136,20 +208,21 @@ impl Popup for ActiveFileSearch {
             return Status::Pending;
         }
         match key.code {
-            KeyCode::Up => self.state.prev(self.options.len()),
-            KeyCode::Down => self.state.next(self.options.len()),
+            KeyCode::Up => self.state.prev(self.buffer.len()),
+            KeyCode::Down => self.state.next(self.buffer.len()),
             KeyCode::Tab => {
                 if self.mode.is_full() {
                     return Status::Dropped;
                 }
                 self.mode = FULL_SEARCH_TITLE;
-                self.tree = tree.shallow_copy_root_tree_path();
+                self.buffer.new_tree(tree.shallow_copy_root_tree_path(), &self.pattern.text);
                 self.collect_data();
             }
             KeyCode::Enter => {
-                if self.options.len() > self.state.selected {
-                    let (path, _, line) = self.options.remove(self.state.selected);
-                    gs.event.push(IdiomEvent::OpenAtLine(path, line));
+                if self.buffer.len() > self.state.selected {
+                    todo!()
+                    // let (path, _, line) = self.options.remove(self.state.selected);
+                    // gs.event.push(IdiomEvent::OpenAtLine(path, line));
                 }
                 return Status::Dropped;
             }
@@ -163,18 +236,19 @@ impl Popup for ActiveFileSearch {
         let Components { gs, .. } = components;
         match event {
             MouseEvent { kind: MouseEventKind::Moved, column, row, .. } => match self.get_option_idx(row, column, gs) {
-                Some(idx) => self.state.select(idx, self.options.len()),
+                Some(idx) => self.state.select(idx, self.buffer.len()),
                 None => return Status::Pending,
             },
             MouseEvent { kind: MouseEventKind::Up(MouseButton::Left), column, row, .. } => {
                 if let Some(index) = self.get_option_idx(row, column, gs) {
-                    let (path, _, line) = self.options.remove(index);
-                    gs.event.push(IdiomEvent::OpenAtLine(path, line));
-                    return Status::Dropped;
+                    todo!()
+                    // let (path, _, line) = self.buffer.remove(index);
+                    // gs.event.push(IdiomEvent::OpenAtLine(path, line));
+                    // return Status::Dropped;
                 }
             }
-            MouseEvent { kind: MouseEventKind::ScrollUp, .. } => self.state.prev(self.options.len()),
-            MouseEvent { kind: MouseEventKind::ScrollDown, .. } => self.state.next(self.options.len()),
+            MouseEvent { kind: MouseEventKind::ScrollUp, .. } => self.state.prev(self.buffer.len()),
+            MouseEvent { kind: MouseEventKind::ScrollDown, .. } => self.state.next(self.buffer.len()),
             _ => return Status::Pending,
         }
         self.force_render(gs);
@@ -182,42 +256,9 @@ impl Popup for ActiveFileSearch {
     }
 
     fn render(&mut self, gs: &mut GlobalState) {
-        match self.clock {
-            Some(clock) => {
-                if clock.elapsed() < WAIT_ON_UPDATE {
-                    return;
-                }
-                self.clock = None;
-                self.options.clear();
-                let tree_path = self.tree.clone();
-                let buffer = Arc::clone(&self.option_buffer);
-                let pattern = self.pattern.text.to_owned();
-                self.join_handle.replace(tokio::task::spawn(async move {
-                    buffer.lock().await.clear();
-                    let mut join_set = tree_path.search_files_join_set(pattern);
-                    while let Some(task_result) = join_set.join_next().await {
-                        if let Ok(result) = task_result {
-                            buffer.lock().await.extend(result);
-                        };
-                    }
-                }));
-            }
-            None => {
-                let Some(handle) = self.join_handle.take() else { return };
-                if !handle.is_finished() {
-                    self.join_handle = Some(handle);
-                }
-
-                // if handle is finished there should not be anything preventing lock
-                let Ok(mut buffer) = self.option_buffer.try_lock() else {
-                    return;
-                };
-                if !buffer.is_empty() {
-                    self.options.extend(buffer.drain(..));
-                }
-                drop(buffer);
-                self.force_render(gs);
-            }
+        if self.buffer.is_running() {
+            self.buffer.flush_results();
+            self.force_render(gs);
         }
     }
 
